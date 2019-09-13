63f7933582c70446f601d726cc0e50ee2a2a7125
blackwidow1088 <chavan.abhijeet1088@gmail.com>
2019-09-13 16:17:49 +0530

    initial update

---
+++ servers/proxy/build_website/precache-manifest.8092803dc3bb048c5cac6b74be283080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ad4778ea002cc446bf5ef4bc537008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90c48ad2ac8ca6ca45ad",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58ae393550e31a5ba5b9",
+    "url": "/static/js/2.a0193c2d.chunk.js"
+  },
+  {
+    "revision": "90c48ad2ac8ca6ca45ad",
+    "url": "/static/js/main.6e40c57e.chunk.js"
+  },
+  {
+    "revision": "8bf540b1ee43d00952d3",
+    "url": "/static/js/runtime~main.e7c01290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
